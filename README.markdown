129103c57a5f9edf92db12b1ed6edebfe1da9d1f
Leevi Graham <leevi@newism.com.au>
2008-12-27 12:49:51 +1100

    Updated README and documented js source code. Added append_toc option

---
+++ README.markdown
@@ -0,0 +1,29 @@
+nsm_TOC.js - A jQuey plugin to create a table of contents from HTML markup.
+===========================================================================
+
+**Author**: [Leevi Graham](leevi@newism.com.au) - Technical Director, [Newism Pty Ltd][]  
+**Version**: 1.0.0  
+**Git Master Repository**: <http://github.com/newism/nsm.toc.jquery_plugin/tree/master>
+
+About
+-----
+
+nsm_TOC.js is a jQuery plugin that generates a table of contents based on header tags &lt;h*n*&gt; in your html markup and adds TOC markers to each heading element.
+
+License
+-------
+
+nsm_TOC.js is currently available for use in all personal or commercial projects under [Creative Commons Attribution-Share Alike 3.0 Unported][] license.
+
+You are free to:
+
+* **Share** - to copy, distribute and transmit the work
+* **Remix** - to adapt the work
+
+Under the following conditions:
+
+* **Attribution** - You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+* **Share Alike** - If you alter, transform, or build upon this work, you may distribute the resulting work only under the same, similar or a compatible license.
+
+[Newism Pty Ltd]: http://newism.com.au/
+[Creative Commons Attribution-Share Alike 3.0 Unported]: http://creativecommons.org/licenses/by-sa/3.0/ 
--- jquery-nsm_TOC.js
+++ jquery-nsm_TOC.js
@@ -1,14 +1,46 @@
-// jquery-nsmTOC.js - A jQuery Plugin
-// Version 1.0.0
-// Written by Leevi Graham - Technical Director - Newism Web Design & Development
-// http://newism.com.au
-// Notes: 
-// Still a work in progress
-// Inspired By:
-// http://code.google.com/p/jqplanize/
-// http://blog.rebeccamurphey.com/2007/12/24/jquery-table-of-contents-plugin-nested/
+/**
+* jQuery.nsm_TOC
+* Copyright (c) 2008-2009 Newmso - leevi(at)newism(dot)com(dot)au | http://newism.com.au
+* Licensed under Creative Commons Attribution-Share Alike 3.0 Unported license. | http://creativecommons.org/licenses/by-sa/3.0/
+* Date: 27 Dec 2008
+*
+* @projectDescription Generate a table of contents based on header tags in your html markup.
+* http://newism.com.au
+*
+* @author Leevi Graham - Technical Director, Newism Pty Ltd
+* @version 1.0.0
+* @requires jQuery 1.2+ http://jquery.com
+* @see http://github.com/newism/nsm.toc.jquery_plugin/tree/master
+*
+* @id jQuery.nsm_TOC
+* @id jQuery.fn.nsm_TOC
+* @param {Object} settings Hash of settings, optional.
+*	 @option {Boolean} append_toc Append the generated TOC to the toc_el
+*	 @option {String, DOMElement, jQuery, Object} toc_el The element that the TOC will be appended to
+*	 @option {String, DOMElement, jQuery} ignore A collection of header elements which are not included in the TOC.
+*	 @option {String} hash_prefix The string prepended to the anchor hash target.
+*	 @option {Number} min_depth The minimun header element to include. Ex: "1" will build the TOC starting with <h1> elements while "2" will build the TOC starting with <h2> elements
+*	 @option {Number} max_depth The max header element to include. Ex: "5" will build the TOC stopping at (but including) <h5> elements while "6" will build the TOC stopping at (but including) <h6> elements
+*	 @option {String} toc_marker_suffix The string added after the toc marker. Ex: If the TOC marker is 1.1.1 and the toc_marker_suffix is "." the final output will be: "1.1.1." If the TOC Marker is 1.2.3 and the number suffix is ")" the final output will be "1.2.3)"
+*	 @option {String} toc_marker_seperator The string that divides the toc_marker levels. Ex: If the TOC marker levels are 1,1 & 1 and the toc_marker_seperator is "." the final output will be: "1.1.1" If the TOC marker levels are 1,1 & 1 and the toc_marker_seperator is "-" the final output will be: "1-1-1"
+*	 @option {Boolean} debug Diplay console messages
+*	 @option {String} header_span_class The class to add to the header toc_marker span element. Ex: <span class="toc-marker">1.2.3</span>
+*	 @option {Boolean} add_top_links Add top links to each heading element in the TOC. Ex: <h1><span class="toc-marker">1</span> Heading 1 <a href="#top">Top</a></h1>
+*	 @option {String} top_link_class The class to add to the top link anchor element. Ex: <a href="#top" class="top">Top</a>
+* @return {jQuery} Returns the same jQuery object, for chaining.
+*
+* @example $('body').nsm_TOC();
+*
+* @example $('body').nsm_TOC({ hash_prefix: "h-"}); // Changes the hash prefix for the TOC links
+* @example $('body').nsm_TOC({ min_depth: 2, max_depth: 5}); // Creates a TOC using heading elments 2-5 nested in the target element
+* @example $('body').nsm_TOC({ add_top_links: false}); // Doesn't add top links to heading elements
+*
+* Notes:
+*  - Influenced by: http://code.google.com/p/jqplanize/ & http://blog.rebeccamurphey.com/2007/12/24/jquery-table-of-contents-plugin-nested/
+*	- Still a work in progress
+*	- Not tested on all platforms
+*/
 
-// create closure
 (function($) {
 	// plugin definition
 	$.fn.nsmTOC = function(options) {
@@ -85,20 +117,20 @@
 
 					//  build the marker
 					for (var l = 0; l <= depth; l++) {
-						toc_marker += levels[l] > 0 ? levels[l] + o.number_separator : '';
+						toc_marker += levels[l] > 0 ? levels[l] + o.toc_marker_separator : '';
 					}
 
 					// get rid of the last seperator
 					toc_marker = prependText = toc_marker.substring(0, toc_marker.length - 1);
 
 					// add a number suffix?
-					prependText = toc_marker + o.number_suffix;
+					prependText = toc_marker + o.toc_marker_suffix;
 
 					// do the titles
 					$self.prepend('<span id="' + o.hash_prefix + toc_marker + '" class="' + o.header_span_class + '">' + prependText + '</span> ');
 					if(o.add_top_links)
 					{
-						$self.append(" <a href='" + o.top_link_target + "' class='" + o.top_link_class + "'>Top</a>");
+						$self.append(" <a href='" + o.top_link_href + "' class='" + o.top_link_class + "'>Top</a>");
 					}
 
 					// do the link
@@ -107,7 +139,10 @@
 				}
 				
 			});
-			$(o.toc_el).append($toc);
+			if(o.append_toc)
+			{
+				$(o.toc_el).append($toc);
+			}
 		});
 
 		function log() {
@@ -126,19 +161,18 @@
 
 	// plugin defaults
 	$.fn.nsmTOC.defaults = {
-		generate_toc:		true, 
-		toc_title:			false,
+		apend_toc:			true, 
 		toc_el:				"body",
 		ignore: 			".toc-ignore",
 		hash_prefix: 		"toc-",
 		min_depth:			1,
 		max_depth: 			6,
-		number_suffix:		".",
-		number_separator: 	".",
+		toc_marker_suffix:		".",
+		toc_marker_separator: 	".",
 		debug: 				true,
 		header_span_class: 	"toc-marker",
 		add_top_links: 		true,
-		top_link_target: 	"#",
+		top_link_href: 		"#",
 		top_link_class: 	"top"
 	};
 
